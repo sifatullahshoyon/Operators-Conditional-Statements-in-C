b97f08eebd88e03ffb4035d853862542cd9560f0
freelancersifat627e <freelancersifat627@gmail.comm>
2025-10-08 17:16:15 +0600

    if else done

---
+++ if_else.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int tk;
+    // take input
+    scanf("%d" , &tk);
+    if(tk >= 100){
+        printf("Burger Khabo\n");
+    } else{
+        printf("Burger Khabo na\n");
+    }
+}
